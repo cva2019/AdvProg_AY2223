dd7f9940744eb07658f0ae79af9fa745d28cb66c
cva2019 <caovananhnd2019@gmail.com>
2023-05-05 00:58:11 +0700

    4

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,6 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
     int randomNumber = rand()% 100 +1;
     return randomNumber;
 }
@@ -28,8 +27,6 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
     int guess;
     cout<<"Enter your guess (1...100) : ";
     cin>>guess;
@@ -80,7 +77,6 @@ string getAnswer(int number, int randomNumber) {
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
     
-    return true;
     if(answer=="Congratulation! You win.")
     {
         return true;
